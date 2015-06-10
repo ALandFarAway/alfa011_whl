16f875d01ee2aaf30ce2c5e9db84bdeacf2657f5
WHL Host <noone@nowhere.com>
2015-06-10 18:10:04 -0400

    Changed module directory to alfa011_whl

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 del *.ncs
 del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -o -h "%userprofile%\Documents\Neverwinter Nights 2" -m alfa011_whl_git -n "C:\Program Files (x86)\Neverwinter Nights 2 - Dedicated Server" .\*.nss -y
+NWNScriptCompiler.exe -e -v1.70 -o -h "%userprofile%\Documents\Neverwinter Nights 2" -m alfa011_whl -n "C:\Program Files (x86)\Neverwinter Nights 2 - Dedicated Server" .\*.nss -y
